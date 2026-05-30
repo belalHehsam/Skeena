f3f53c17d9c94d6d3f70af96ffd251fa7d55d3c3
Ammar Ahmed <ammarahmed1263@gmail.com>
2026-05-31 00:03:20 +0300

    testing

--- src/components/providers/index.tsx
+++ src/components/providers/index.tsx
@@ -2,6 +2,7 @@ import { type ReactNode } from "react";
 import TanstackQueryProvider from "@/components/providers/TanstackQueryProvider";
 import { DarkModeProvider } from "@/components/context/DarkModeContext";
 import { Toaster } from 'sonner'
+import SocketProvider from "./SocketProvider";
 
 type ProvidersProps = {
     children: ReactNode;
@@ -11,8 +12,11 @@ function Providers({ children }: ProvidersProps) {
     return (
         <DarkModeProvider>
             <Toaster richColors position="bottom-right" />
-
-            <TanstackQueryProvider>{children}</TanstackQueryProvider>
+            <TanstackQueryProvider>
+                <SocketProvider>
+                    {children}
+                </SocketProvider>
+            </TanstackQueryProvider>
         </DarkModeProvider>
     );
 }

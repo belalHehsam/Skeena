57cf9b6eb2df034c933d86dd95542bae75615422
Ammar Ahmed <ammarahmed1263@gmail.com>
2026-06-07 01:59:23 +0300

    chore(shadcn): unify cn import through the project

--- src/components/feedbacks/NoData.tsx
+++ src/components/feedbacks/NoData.tsx
@@ -1,7 +1,7 @@
 import type { ReactNode } from "react";
 import { useTranslation } from "react-i18next";
 import { FiInbox } from "react-icons/fi";
-import { cn } from "@/lib/shadcnUtils";
+import { cn } from "@/lib/utils";
 
 export interface NoDataProps {
   messageKey?: TranslationKey;
--- src/components/feedbacks/Spinner.tsx
+++ src/components/feedbacks/Spinner.tsx
@@ -1,4 +1,4 @@
-import { cn } from "@/lib/shadcnUtils";
+import { cn } from "@/lib/utils";
 
 const Spinner = ({ className = "" }) => {
   return (

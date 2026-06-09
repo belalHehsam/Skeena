04ba87dd4d9fbd19945e124e955c2c946a075b61
Ammar Ahmed <ammarahmed1263@gmail.com>
2026-06-09 17:28:50 +0300

    feat(i18n): configure english and arabic translations for notifications

--- src/translations/ar/index.ts
+++ src/translations/ar/index.ts
@@ -1,4 +1,7 @@
 import common from "./common.json";
+import notifications from "./notifications.json";
+
 export const ar = {
     common,
+    notifications,
 } as const;
--- src/translations/en/index.ts
+++ src/translations/en/index.ts
@@ -1,4 +1,7 @@
 import common from "./common.json";
+import notifications from "./notifications.json";
+
 export const en = {
     common,
+    notifications,
 } as const;
